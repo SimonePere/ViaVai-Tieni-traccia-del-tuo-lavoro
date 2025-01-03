940d74d1da6023726542a50e9c9a8c8c31007a10
SimonePere <simonemele6596@gmail.com>
2025-01-03 12:23:11 +0100

    Added components folder with UI subfolder and components

---
+++ client/app/components/ui/Dashboard.tsx
@@ -0,0 +1,110 @@
+/** @format */
+
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Dashboard() {
+  return (
+    <>
+      <nav className="fixed top-0 z-50 w-full">
+        <div className="px-3 py-3 lg:px-5 lg:pl-3">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <button
+                type="button"
+                aria-controls="logo-sidebar"
+                aria-expanded="false"
+                className="inline-flex items-center p-2 text-sm rounded-lg sm:hidden"
+              >
+                <span className="sr-only">Open sidebar</span>
+                <svg
+                  className="w-6 h-6"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    clipRule="evenodd"
+                    fillRule="evenodd"
+                    d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+                  ></path>
+                </svg>
+              </button>
+              <Link href="https://flowbite.com" className="flex ms-2 md:me-24">
+                <Image
+                  src="/logo.svg"
+                  alt="FlowBite Logo"
+                  width={32}
+                  height={32}
+                  className="me-3"
+                />
+                <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap">
+                  Flowbite
+                </span>
+              </Link>
+            </div>
+            <div className="flex items-center">
+              <div className="ms-3">
+                <button
+                  type="button"
+                  aria-expanded="false"
+                  className="flex text-sm rounded-full"
+                >
+                  <span className="sr-only">Open user menu</span>
+                  <Image
+                    src="/profile-picture.jpg"
+                    alt="User photo"
+                    width={32}
+                    height={32}
+                    className="rounded-full"
+                  />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <aside
+        id="logo-sidebar"
+        className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform"
+        aria-label="Sidebar"
+      >
+        <div className="h-full px-3 pb-4 overflow-y-auto">
+          <ul>
+            <li>
+              <Link className="flex items-center p-2 rounded-lg" href="#">
+                <svg
+                  className="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 21"
+                >
+                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                </svg>
+                <span className="ms-3">Dashboard</span>
+              </Link>
+            </li>
+            <li>
+              <Link className="flex items-center p-2 rounded-lg" href="#">
+                <svg
+                  className="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 18 18"
+                >
+                  <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
+                </svg>
+                <span className="ms-3">Kanban</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </aside>
+    </>
+  );
+}
